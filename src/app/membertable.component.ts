0c32b0cf59f044ee7a850d5f38d8e26912c5f6c5
Ancy <lazuliroseancy@gmail.com>
2017-12-15 10:35:44 +0900

    Add error and showselected name

--- src/app/membertable.component.ts
+++ src/app/membertable.component.ts
@@ -6,27 +6,46 @@ import { Member } from './member';
   templateUrl: './membertable.component.html',
   styleUrls: ['./membertable.component.css']
 })
-
 export class MembertableComponent  { 
-		members=Member.members;
-		selectedMember:Member[];
-		constructor(){this.selectedMembers = new Array();};
-		onSelect(member:Member){
+    members = Member.members;
+    selectedMembers: Member[];
+    overflow: boolean;
+    whoisselected: string;
+
+    constructor() {
+        this.selectedMembers = new Array()
+        this.overflow = false;
+        this.whoisselected = "";
+
+    };
+
+    onSelect(member: Member) {
         let selectedindex = this.isSelected(member);
-         if (selectedindex >=0) {
+        if (selectedindex >= 0) {
             this.selectedMembers.splice(selectedindex, 1);
-         }
-        else {
-            this.selectedMembers.push(member);  
+            this.overflow = false;
+        } else if (this.selectedMembers.length > 3) {
+            this.overflow = true;
         }
+        else {
+            this.selectedMembers.push(member);
+        }
+        this.whoisselected = this.showWhoisSelected();
     }
-		isSelected(themember:Member):number{
-		for (let i=0; i<this.selectedMembers.length; i++) {
-		if (this.selectedMembers[i]==themember) {
-				return i;
-				}
+
+isSelected(themember:Member):number{
+	for (let i=0; i<this.selectedMembers.length; i++) {
+	if (this.selectedMembers[i]==themember) {
+			return i;
 			}
 		}
 		return -1;
 	}
+showWhoisSelected(): string {
+        let whois = "";
+        for (let member of this.selectedMembers) {
+            whois += member.name + "さん, ";
+        }
+        return whois.substr(0, whois.length - 2) + "が選ばれました";
+    }
 }
